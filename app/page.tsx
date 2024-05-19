5354dcb1739031b30e3a0f2dae316aafb58f8fe9
seacoding1 <somdays01@gmail.com>
2024-05-19 18:50:38 +0900

    카드 라이트/다크 버전, hover

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,34 @@
 export default function Home() {
-  return <main className="text-"> </main>;
+  return (
+    <main className="bg-gray-100 h-screen flex items-center
+                      justify-center p-5 dark:bg-gray-700">
+      <div className="bg-white w-full shadow-lg p-5 rounded-3xl
+                        max-w-screen-sm dark:bg-gray-600">
+        <div className="flex justify-between items-center">
+          <div className="flex flex-col">
+            <span className="text-gray-600 font-semibold -mb-1
+            dark:text-gray-300">In transit</span>
+            <span className="text-4xl font-semibold dark:text-white">Coolblue</span>
+          </div>
+          <div className="size-12 rounded-full bg-orange-400" />
+        </div>
+        <div className="my-2">
+          <span className="bg-green-400 text-white uppercase
+          px-2.5 py-1.5 mr-2 text-xs font-medium rounded-full
+          hover:bg-green-500 transition">Today</span>
+          <span className="font-medium dark:text-gray-100">9:30~10:30</span>
+        </div>
+        <div className="relative">
+          <div className="bg-gray-200 w-full h-2 rounded-full absolute"/>
+          <div className="bg-green-400 h-2 rounded-full w-2/3 absolute"/>
+        </div>
+        <div className="flex justify-between items-center mt-5 text-gray-600 dark:text-gray-300">
+          <span>Expected</span>
+          <span>Sorting center</span>
+          <span>In transit</span>
+          <span className="text-gray-400 dark-gray-500 ">Delivered</span>
+        </div>
+      </div>
+    </main>
+  );
 }
