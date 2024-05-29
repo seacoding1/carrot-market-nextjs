2a383a1b36ea7f36fc9a7f0d4ffcbdd58ebd0df6
seacoding1 <somdays01@gmail.com>
2024-05-29 19:05:46 +0900

    반응형, has, *

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,25 @@
 export default function Home() {
   return (
-    <main className="bg-gray-100 h-screen flex items-center
-                      justify-center p-5 dark:bg-gray-700">
-      <div className="bg-white w-full shadow-lg p-5 rounded-3xl
-                        max-w-screen-sm dark:bg-gray-600">
-        <div className="flex justify-between items-center">
-          <div className="flex flex-col">
-            <span className="text-gray-600 font-semibold -mb-1
-            dark:text-gray-300">In transit</span>
-            <span className="text-4xl font-semibold dark:text-white">Coolblue</span>
-          </div>
-          <div className="size-12 rounded-full bg-orange-400" />
-        </div>
-        <div className="my-2">
-          <span className="bg-green-400 text-white uppercase
-          px-2.5 py-1.5 mr-2 text-xs font-medium rounded-full
-          hover:bg-green-500 transition">Today</span>
-          <span className="font-medium dark:text-gray-100">9:30~10:30</span>
-        </div>
-        <div className="relative">
-          <div className="bg-gray-200 w-full h-2 rounded-full absolute"/>
-          <div className="bg-green-400 h-2 rounded-full w-2/3 absolute"/>
-        </div>
-        <div className="flex justify-between items-center mt-5 text-gray-600 dark:text-gray-300">
-          <span>Expected</span>
-          <span>Sorting center</span>
-          <span>In transit</span>
-          <span className="text-gray-400 dark-gray-500 ">Delivered</span>
-        </div>
+    <main className="bg-gray-100 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100 h-screen flex items-center justify-center p-5">
+      <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex
+                      flex-col md:flex-row gap-2 sm:*:outline-none 
+                      ring ring-transparent transition-shadow has-[:invalid]:ring-red-100
+                      ">
+        <input
+          className="w-full rounded-full h-10 bg-gray-200 pl-5 ring ring-transparent
+          focus:ring-green-500 focus:ring-offset-2 transition-shadow
+          placeholder:drop-shadow invalid:focus:ring-red-500 peer"
+          type="text"
+          required
+          placeholder="Email address"
+        />
+        
+        <span className="text-red-500 font-medium hidden peer-invalid:block ">
+          Email is required.
+        </span>
+        <button className="text-white py-2 rounded-full active:scale-90  transition-transform font-medium md:px-10 bg-black ">
+          Log in
+        </button>
       </div>
     </main>
   );
