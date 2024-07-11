97299ae3baae18fdd28dbc2d6082ed4f862713f5
seacoding1 <somdays01@gmail.com>
2024-07-11 15:49:07 +0900

    tailwind css 배우기

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,20 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <main className="bg-gray-100 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100 h-screen flex items-center justify-center p-5">
-      <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex
-                      flex-col md:flex-row gap-2 sm:*:outline-none 
-                      ring ring-transparent transition-shadow has-[:invalid]:ring-red-100
-                      ">
-        <input
-          className="w-full rounded-full h-10 bg-gray-200 pl-5 ring ring-transparent
-          focus:ring-green-500 focus:ring-offset-2 transition-shadow
-          placeholder:drop-shadow invalid:focus:ring-red-500 peer"
-          type="text"
-          required
-          placeholder="Email address"
-        />
-        
-        <span className="text-red-500 font-medium hidden peer-invalid:block ">
-          Email is required.
-        </span>
-        <button className="text-white py-2 rounded-full active:scale-90  transition-transform font-medium md:px-10 bg-black ">
-          Log in
-        </button>
+    <div className="flex flex-col items-center justify-between min-h-screen">
+      <div className="my-auto">
+        <span className="text-9xl">🥕</span>
+        <h1 className="text-4xl">당근</h1>
+        <h2 className="text-2xl">당근 마켓에 어서오세요!</h2>
       </div>
-    </main>
+      <div>
+        <Link href="/create-account">시작하기</Link>
+        <div>
+            <span>이미 계정이 있나요?</span>
+            <Link href="/login"></Link>
+        </div>
+      </div>
+    </div>
   );
 }
